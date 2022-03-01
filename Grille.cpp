333ffa4e5eec9d88c20e37a8e73788894f47cc48
SeleneTononETPA <selene.t.etpa@gmail.com>
2022-03-01 11:28:37 +0100

    getsetgrille

--- Grille.cpp
+++ Grille.cpp
@@ -5,31 +5,28 @@
 #include <iostream>
 
     Grille::Grille() : _nombreColonnes(3), _nombreLignes(3) { 
-        // à compléter
+        _tableau = std::vector<char>(9,' ');
+        resetContent(); 
     }
     
     Grille::Grille(int nbColonnes, int nbLignes) : _nombreLignes(nbLignes), _nombreColonnes(nbColonnes) {
-        // à compléter
+        _tableau = std::vector<char>(nbColonnes * nbLignes,' ');
     }
 
     // renvoie le contenu de la case de coordonnées x,y dans [0;2]
-    char Grille::getContent(int x, int y){ 
-        // à compléter
-    }    
+    char Grille::getContent(int x, int y){ return _tableau[x+(y)*_nombreColonnes]; }    
 
         // ajoute un contenu dans une case de coordonnées x,y dans [0;2]
         // le contenu est un entier non nul
     void Grille::setContent(int x, int y, char nouveauContenu){
-        // à compléter
+        _tableau[x+(y)*_nombreColonnes] = nouveauContenu;
     }
         
     void Grille::affiche(){
         // à compléter
     }
 
-    void Grille::resetContent(){
-        // à compléter
-    }
+    void Grille::resetContent(){for(int i=0;i<_nombreLignes*_nombreColonnes;i++) _tableau[i] = ' ';}
 
 
 #endif
